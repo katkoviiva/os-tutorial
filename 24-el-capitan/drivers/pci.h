c8929769e15d6068f6fa7179cba3d8dfb4999190
Jussi Asikainen <47023770+katkoviiva@users.noreply.github.com>
2026-09-15 15:41:21 +0300

    Enhance PCI header with device struct and init function
    Added PCI initialization function and device structure.

--- 24-el-capitan/drivers/pci.h
+++ 24-el-capitan/drivers/pci.h
@@ -1,9 +1,45 @@
-#ifndef PCI_H
-#define PCI_H
+#ifndef _PCI_H
+#define _PCI_H 1
 
 #include "../cpu/type.h"
 
-uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
-void check_all_pci_buses();
+int pci_init (void);
+
+typedef struct _pciDev_t
+{
+	uint16_t vendor_id;
+	uint16_t device_id;
+	
+	uint16_t command;
+	uint16_t status;
+	
+	uint8_t revision_id;
+	uint8_t interface;
+	uint8_t sub_class;
+	uint8_t base_class;
+	
+	uint8_t cache_line_size;
+	uint8_t latency_timer;
+	uint8_t header_type;
+	uint8_t bist;
+
+	uint8_t bus;
+	uint8_t dev;
+	uint8_t func;
+
+	uint8_t irq;
+	uint32_t base[6];
+	uint32_t size[6];
+	uint8_t type[6];
+	uint32_t rom_base;
+	uint32_t rom_size;
+
+	uint16_t subsys_vendor;
+	uint16_t subsys_device;
+
+	uint8_t current_state;
+} pciDev_t;
+
+pciDev_t *pciGetDeviceById(uint16_t vendor, uint32_t device, int index);
 
 #endif
